8c97c37eb6200d46fb2f1d85d7e6b3c0a07baf63
Jackson Hardaker <jackson@bitdrift.io>
2024-12-13 15:06:55 -0500

    move under header

--- packages/react-native/src/log.test.ts
+++ packages/react-native/src/log.test.ts
@@ -1,11 +1,11 @@
-/* eslint-disable no-useless-escape */
-
 // capture-es - bitdrift's ES SDK
 // Copyright Bitdrift, Inc. All rights reserved.
 //
 // Use of this source code is governed by a source available license that can be found in the
 // LICENSE file or at:
 // https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
+
+/* eslint-disable no-useless-escape */
 
 import { log } from './log';
 import Native from './NativeBdReactNative';
