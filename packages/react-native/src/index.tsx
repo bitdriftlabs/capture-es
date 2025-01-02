b296e677d66f9ee5db3334ef50549b1ef347e024
Jackson Hardaker <jackson@bitdrift.io>
2025-01-02 16:41:33 -0500

    expose getDeviceID for android and generateDeviceCode

--- packages/react-native/src/index.tsx
+++ packages/react-native/src/index.tsx
@@ -1,6 +1,10 @@
 import { NativeModules, Platform } from 'react-native';
 import { log, type SerializableLogFields } from './log';
 import { InitOptions, SessionStrategy } from './NativeBdReactNative';
+import NativeBdReactNative from './NativeBdReactNative';
+
+let api_url: string;
+let api_key: string;
 
 const LINKING_ERROR =
   `The package '@bitdrift/react-native' doesn't seem to be linked. Make sure: \n\n` +
@@ -33,6 +37,9 @@ export function init(
   sessionStrategy: SessionStrategy,
   options?: InitOptions
 ): void {
+  api_url = options?.url ?? 'api.bitdrift.io';
+  api_key = key;
+
   return BdReactNative.init(key, sessionStrategy, options);
 }
 
@@ -54,4 +61,24 @@ export function warn(message: string, fields?: SerializableLogFields): void {
 
 export function error(message: string, fields?: SerializableLogFields): void {
   return log(4, message, fields);
+}
+
+export async function getDeviceID(): Promise<string> {
+  return NativeBdReactNative.getDeviceID();
+}
+
+export async function generateDeviceCode(): Promise<string> {
+  const deviceId = await getDeviceID();
+  const body = JSON.stringify({ device_id: deviceId });
+
+  return fetch(`${api_url}:443/v1/device/code`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-bitdrift-api-key': api_key,
+    },
+    body,
+  })
+    .then((res) => res.json())
+    .catch((err) => err);
 }
