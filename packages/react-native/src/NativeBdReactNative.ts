c62351588f73a9be9f0792c335aa9616d93f17ea
Snow Pettersen <snowp@lyft.com>
2025-01-03 10:52:55 -0800

    pass the correct string

--- packages/react-native/src/NativeBdReactNative.ts
+++ packages/react-native/src/NativeBdReactNative.ts
@@ -9,7 +9,7 @@ import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
 export type LogFields = { [key: string]: string };
-export enum SessionStrategy { Activity, Fixed };
+export enum SessionStrategy { Activity = 'activity', Fixed = 'fixed'  };
 export type InitOptions = { url?: string; enableNetworkInstrumentation?: boolean };
 
 export interface Spec extends TurboModule {
