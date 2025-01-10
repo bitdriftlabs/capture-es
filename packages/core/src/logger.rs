a44ff1d00be6a065465f1f784ef18dfde682e918
Snow Pettersen <snowp@lyft.com>
2025-01-10 08:42:03 -0800

    add a basic rust integration test for logger used by electron

--- packages/core/src/logger.rs
+++ packages/core/src/logger.rs
@@ -12,6 +12,10 @@
 // LICENSE file or at:
 // https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
 
+#[cfg(test)]
+#[path = "./integration_test.rs"]
+mod integration_test;
+
 use crate::SessionStrategy;
 use bd_client_common::error::handle_unexpected;
 use bd_key_value::Storage;
