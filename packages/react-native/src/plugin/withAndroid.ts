b96961c49ab316f43409cf04784fc1af3857b350
Snow Pettersen <snowp@lyft.com>
2025-01-24 13:43:50 -0800

    update to 0.16.10

--- packages/react-native/src/plugin/withAndroid.ts
+++ packages/react-native/src/plugin/withAndroid.ts
@@ -22,7 +22,7 @@ const withBitdriftAppBuildGradle: ConfigPlugin<PluginProps | void> = (
         // Add the capture-plugin at the very top of the file.
         config.modResults.contents =
           `plugins {
-    id 'io.bitdrift.capture-plugin' version '0.16.9'
+    id 'io.bitdrift.capture-plugin' version '0.16.10'
 }
 
 ` + config.modResults.contents;
