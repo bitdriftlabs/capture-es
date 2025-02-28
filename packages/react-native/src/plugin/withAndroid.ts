d48138b99fc2f251c26451965613845fcc7b271a
Fran Aguilera <fran@bitdrift.io>
2025-02-28 10:16:41 +0100

    Update capture-sdk to 0.17.5

--- packages/react-native/src/plugin/withAndroid.ts
+++ packages/react-native/src/plugin/withAndroid.ts
@@ -22,7 +22,7 @@ const withBitdriftAppBuildGradle: ConfigPlugin<PluginProps | void> = (
         // Add the capture-plugin at the very top of the file.
         config.modResults.contents =
           `plugins {
-    id 'io.bitdrift.capture-plugin' version '0.16.11'
+    id 'io.bitdrift.capture-plugin' version '0.17.5'
 }
 
 ` + config.modResults.contents;
