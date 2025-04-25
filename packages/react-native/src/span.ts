f7d4a1f120ac30529aed8b8cea7bb36b0ef5d885
Snow Pettersen <snow@bitdrift.io>
2025-04-25 11:53:04 -0700

    header

--- packages/react-native/src/span.ts
+++ packages/react-native/src/span.ts
@@ -1,3 +1,10 @@
+// capture-es - bitdrift's ES SDK
+// Copyright Bitdrift, Inc. All rights reserved.
+//
+// Use of this source code is governed by a source available license that can be found in the
+// LICENSE file or at:
+// https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
+
 import { SerializableLogFields, logInternal, LogLevel } from './log';
 import { v4 as uuidv4 } from 'uuid';
 
