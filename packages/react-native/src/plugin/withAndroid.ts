e0cedb63209da28e4fc25cb238436b35dd5f8a1f
Snow Pettersen <snow@bitdrift.io>
2025-08-21 19:38:22 -0400

    fixes

--- packages/react-native/src/plugin/withAndroid.ts
+++ packages/react-native/src/plugin/withAndroid.ts
@@ -5,8 +5,8 @@
 // LICENSE file or at:
 // https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
 
-import { withPlugins } from '@expo/config-plugins';
-import { withAppBuildGradle, withSettingsGradle } from '@expo/config-plugins';
+import { withPlugins } from 'expo/config-plugins';
+import { withAppBuildGradle, withSettingsGradle } from 'expo/config-plugins';
 import type { ConfigPlugin } from '@expo/config-plugins';
 import PluginProps from './config';
 
