91082113a929941e6abc0a65380b4a1065726f50
FranAguilera <4230063+FranAguilera@users.noreply.github.com>
2025-08-27 23:52:45 +0000

    Update React Native version with Capture SDK 0.18.65725 and bump RN package to 0.7.2

--- packages/react-native/src/plugin/withAndroid.ts
+++ packages/react-native/src/plugin/withAndroid.ts
@@ -22,7 +22,7 @@ const withBitdriftAppBuildGradle: ConfigPlugin<PluginProps | void> = (
         // Add the capture-plugin at the very top of the file.
         config.modResults.contents =
           `plugins {
-    id 'io.bitdrift.capture-plugin' version '0.18.6'
+    id 'io.bitdrift.capture-plugin' version '0.18.65725'
 }
 
 ` + config.modResults.contents;
